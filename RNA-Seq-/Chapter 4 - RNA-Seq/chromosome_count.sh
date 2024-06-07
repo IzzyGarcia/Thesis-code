e962b373f819170d0849006c0dfa9f6db46abf8d
IzzyGarcia <131690062+IzzyGarcia@users.noreply.github.com>
2024-06-07 14:23:20 +0100

    Delete RNA-Seq- directory

--- RNA-Seq-/Chapter 4 - RNA-Seq/chromosome_count.sh
+++
@@ -1,8 +0,0 @@
-#Create an index file
-samtools index input.bam
-
-#count per chromosome
-samtools idxstats input.bam| awk '{print $1" "$3}' > Chromosome_count_samplename.txt
-
-#or loop
-for sample in *.bam;do echo -en $sample"\n"; name=$(basename $sample ".bam"); ~/Tools/samtools-1.14/samtools index -@ 10 $sample; ~/Tools/samtools-1.14/samtools idxstats $sample | awk '{print $1" "$3}' >Chromosome_stats_${name}.txt; done
